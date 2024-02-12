2292c213c5e5db4beab3f62f1b29719019040694
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-12 13:33:00 +0100

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#docker compose safe
+if command -v docker-compose &>/dev/null; then
+    docker_compose="docker-compose"
+elif docker --help | grep -q "compose"; then
+    docker_compose="docker compose"
+fi
+
+cd ~/powerloom
+$docker_compose down
