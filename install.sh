0665dd81485c65273630834735b8bae1400d56f8
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-28 16:39:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~
-git clone https://github.com/PowerLoom/snapshotter-lite powerloom-testnet
-cd powerloom-testnet
+git clone https://github.com/PowerLoom/snapshotter-lite-v2.git powerloom-pre-mainnet-simulation
+cd powerloom-pre-mainnet-simulation
 ./build.sh
