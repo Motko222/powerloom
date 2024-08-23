75b111da46c91b00f943e37abdb6f9ba1ed2efbf
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-23 22:07:30 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,8 +10,8 @@ fi
 source ~/.bash_profile
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-docker_status=$(docker inspect powerloom-pre-mainnet-simulation-snapshotter-lite-v2-1 | jq -r .[].State.Status)
-foldersize=$(du -hs ~/powerloom-pre-mainnet-simulation | awk '{print $1}')
+docker_status=$(docker inspect powerloom-pre-mainnet_-snapshotter-lite-v2_1 | jq -r .[].State.Status)
+foldersize=$(du -hs ~/powerloom-pre-mainnet | awk '{print $1}')
 id=$POWERLOOM_ID
 network=testnet
 chain="testnet v2"
